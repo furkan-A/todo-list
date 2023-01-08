eb3e538160b6e138af7c5440cb6d529d90bbae42
Furkan Aktaş <furkanakt21@gmail.com>
2023-01-08 23:16:36 +0300

    initialize commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>todos</p>
+      </header>
+      <div className="App-"></div>
+      <footer className="App-footer"></footer>
+    </div>
+  );
+}
+
+export default App;
